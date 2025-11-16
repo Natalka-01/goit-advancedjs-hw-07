d0fb736c70b5f2ae883c09ae17850aed74612208
Natalka-01 <nataliia.dubikova@gmail.com>
2025-11-16 18:24:12 +0100

    HW07

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,78 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+    console.log(`Key створено з сигнатурою: ${this.signature.toFixed(5)}`);
+  }
+
+  
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+    console.log('Person створено і має ключ.');
+  }
+
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+
+abstract class House {
+  
+  protected door: boolean = false;
+  
+  protected key: Key;
+  
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+    console.log('House створено.');
+  }
+
+  
+  abstract openDoor(key: Key): void;
+
+  
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Двері відкриті. Людина зайшла до будинку.');
+    } else {
+      console.log('Двері зачинені. Людина не може увійти.');
+    }
+  }
+
+  
+  public getTenants(): Person[] {
+    return this.tenants;
+  }
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Ключі збігаються. Двері відкриті!');
+    } else {
+      this.door = false;
+      console.log('Ключі НЕ збігаються. Двері залишаються зачиненими.');
+    }
+  }
+}
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
